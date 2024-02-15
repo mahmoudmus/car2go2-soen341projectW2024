919fb9942cac77bac766b7cf136a900f25cddb93
mahutt <tommy.mahut@gmail.com>
2024-02-14 20:03:48 -0500

    Created base express application.

---
+++ app/models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    user_name: { type: String, required: true, maxLength: 100 },
+});
+
+module.exports = mongoose.model('User', UserSchema);
