5ee62c1381cb234e06e7e0998d022a569a245f06
mahutt <tommy.mahut@gmail.com>
2024-02-24 13:59:39 -0500

    Created vehicle row template and added bs icons dependency.

--- app/app.js
+++ app/app.js
@@ -41,6 +41,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/')));
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap-icons/')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
