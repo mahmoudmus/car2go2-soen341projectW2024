4e17d7c13f6ae97044dc27d324d439639c4b9340
mahutt <tommy.mahut@gmail.com>
2024-02-26 16:37:02 -0500

    Created first unit test, and set up GitHub workflow file.

---
+++ app/spec/userController.spec.js
@@ -0,0 +1,27 @@
+const User = require('../models/user');
+const userController = require('../controllers/userController');
+
+describe('User Controller', function () {
+    it('should fetch all usernames and pass them to be rendered in the user list', async function () {
+        spyOn(User, 'find').and.returnValue(
+            Promise.resolve([
+                { user_name: 'Azal Al-Mashta' },
+                { user_name: 'Mahmoud Mustafa' },
+            ])
+        );
+
+        const res = {
+            render: jasmine.createSpy(),
+        };
+
+        await userController.user_list({}, res, () => {});
+
+        expect(User.find).toHaveBeenCalledWith({}, 'user_name');
+        expect(res.render).toHaveBeenCalledWith('user_list', {
+            user_list: [
+                { user_name: 'Azal Al-Mashta' },
+                { user_name: 'Mahmoud Mustafa' },
+            ],
+        });
+    });
+});
