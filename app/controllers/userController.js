457c815a93245000f6895679b820643169b0249b
mahutt <tommy.mahut@gmail.com>
2024-02-28 18:57:33 -0500

    Defined vehicles router and refactored users router for consistency.

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 const asyncHandler = require('express-async-handler');
 
 // Display list of all users.
-exports.user_list = asyncHandler(async (req, res, next) => {
+exports.readAllUsers = asyncHandler(async (req, res, next) => {
     const allUsers = await User.find({}, 'user_name');
     res.render('user_list', { user_list: allUsers });
 });
---
+++ app/controllers/vehicleController.js
@@ -0,0 +1,27 @@
+const User = require('../models/user');
+const asyncHandler = require('express-async-handler');
+
+exports.createVehicle = asyncHandler(async (req, res, next) => {
+    // @todo tsk-12007
+    res.sendStatus(404);
+});
+
+exports.readAllVehicles = asyncHandler(async (req, res, next) => {
+    // @todo tsk-12008
+    res.sendStatus(404);
+});
+
+exports.readVehicle = asyncHandler(async (req, res, next) => {
+    // @todo tsk-12008
+    res.sendStatus(404);
+});
+
+exports.updateVehicle = asyncHandler(async (req, res, next) => {
+    // @todo tsk-12009
+    res.sendStatus(404);
+});
+
+exports.deleteVehicle = asyncHandler(async (req, res, next) => {
+    // @todo tsk-12010
+    res.sendStatus(404);
+});
