11886ef323b1f0109443809652d876a517ede2a9
Royaleagle04 <55101369+Royaleagle04@users.noreply.github.com>
2024-02-29 20:46:44 -0500

    Added status for reservation schema

--- app/models/reservation.js
+++ app/models/reservation.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const reservationSchema = new Schema({
-    reservation_id: { type: String, required: true, maxLength: 20 },
+    id: { type: String, required: true, maxLength: 20 },
     vehicle_id: { type: String, required: true, maxLength: 20 },
     user_id: {type: String, required: true, maxLength: 20 },
     start_time: { type: Date, required: true, default: Date.now },
@@ -10,7 +10,7 @@ const reservationSchema = new Schema({
     // TBD
     pickup_location_id: {type: String, required: true },
     dropoff_location_id: {type: String, required: true },
-
+    status: {type: String, default: 'Pending'}
 });
 
 module.exports = mongoose.model('Reservation', reservationSchema);
