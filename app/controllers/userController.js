3d0501acd079c4dbba453977e3949e3326b12f1b
mahutt <tommy.mahut@gmail.com>
2024-03-10 12:57:55 -0400

    Added appropriate error and confirmation messages when creating/updating users.

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -52,7 +52,6 @@ exports.signUp = asyncHandler(async (req, res, next) => {
 
 exports.createUser = asyncHandler(async (req, res, next) => {
     const { name, email, age, address, type, hash } = req.body;
-    console.log({ name, email, age, address, hash });
     if (age < 18) {
         return res.status(400).json({
             message: 'Users must be atleast 18 years of age.',
@@ -140,22 +139,38 @@ exports.updateProfile = asyncHandler(async (req, res, next) => {
 
 exports.updateUser = asyncHandler(async (req, res, next) => {
     const user = await User.findById(req.params.id);
-    if (req.user && req.user.type === 'admin') {
-        const { name, age, email, address, type, hash } = req.body;
-        user.name = name;
-        user.age = age;
-        user.email = email;
-        user.address = address;
-        user.type = type;
+    if (!(req.user && req.user.type === 'admin')) {
+        return res
+            .status(401)
+            .json({ message: 'You do not have admin privileges.' });
+    }
 
-        if (hash) {
-            user.hash = hash;
-        }
+    const { name, age, email, address, type, hash } = req.body;
+    if (age < 18) {
+        return res
+            .status(400)
+            .json({ message: 'Users must be atleast 18 years of age.' });
+    }
+    user.name = name;
+    user.age = age;
+    user.email = email;
+    user.address = address;
+    user.type = type;
+    if (hash) {
+        user.hash = hash;
+    }
 
+    try {
         const updatedUser = await user.save();
         res.status(200).json({ updatedUser });
-    } else {
-        res.status(401).json({ message: 'You do not have admin privileges.' });
+    } catch (e) {
+        if (e.code === 11000) {
+            res.status(400).json({
+                message: `${email} is already in use.`,
+            });
+        } else {
+            res.status(500).json({ message: 'An unexpected error occurred.' });
+        }
     }
 });
 
