ac42bfa2bf982e7dd19a5417a40a10178d2134bf
mahutt <tommy.mahut@gmail.com>
2024-03-19 01:45:51 -0400

    Created the vehicle details button & modal.

--- app/routes/branch.js
+++ app/routes/branch.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const branchController = require('../controllers/branchController');
 
 router.get('', branchController.getAllBranches);
+router.get('/:id', branchController.getBranch);
 
 module.exports = router;
