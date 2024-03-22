dd9406d2a45ff6d6994456247439080679739cbe
atahle <adamtahle7@gmail.com>
2024-03-22 02:30:49 -0400

    Created the booking confirmation modal

--- app/public/javascripts/branchPicker.component.js
+++ app/public/javascripts/branchPicker.component.js
@@ -31,7 +31,7 @@ class BranchPicker extends HTMLElement {
         if (!this.branches || this.select.value === '') {
             return null;
         }
-        for (branch of this.branches) {
+        for (const branch of this.branches) {
             if (branch._id === this.select.value) {
                 return branch;
             }
