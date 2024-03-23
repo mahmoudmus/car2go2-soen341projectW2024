861cd8980583e371adc6cc05c116c9d638efcbb8
mahutt <tommy.mahut@gmail.com>
2024-03-23 15:51:27 -0400

    Created transaction model.

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
-const Reservation = require('./reservation');
 
 const UserSchema = new Schema({
     name: { type: String, required: true },
