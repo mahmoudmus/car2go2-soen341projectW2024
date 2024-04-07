af20fb2bd27700ea09df7522b3e4c41b9ea1c6bc
atahle <adamtahle7@gmail.com>
2024-04-06 21:31:27 -0400

    completed the code for the deletion of cards and saving liked vehicles in an array

--- app/public/javascripts/datingCard.component.js
+++ app/public/javascripts/datingCard.component.js
@@ -8,32 +8,32 @@ class DatingCard extends HTMLElement {
             <div class="card mb-3" style="width: 42rem;">
                     
                 <img
-                    src="<%= vehicle.imageUrl %>"
+                    src="${vehicle.imageUrl}"
                     alt="Car Image"
                     style="width: 30rem;"
                 />
                 <div class="card-body">
-                    <h4 class="card-title"><%= vehicle.details.model %></h4>
-                    <p class="card-text">I'm a <%= vehicle.details.colour %>, <%= vehicle.type %> <%= vehicle.category %> built by
-                    <%= vehicle.details.make %> in <%= vehicle.details.year %>.
+                    <h4 class="card-title">${vehicle.details.model}</h4>
+                    <p class="card-text">I'm a ${vehicle.details.colour}, ${vehicle.type} ${vehicle.category} built by
+                    ${vehicle.details.make} in ${vehicle.details.year}.
                     </p>
                     <div class="row">
                         <div class="col-md-4">
                             <ul>
-                                <li><b>Price: </b><%= vehicle.dailyPrice %>$/day</li>
-                                <li><b>Mileage: </b> <%= vehicle.details.mileage %> km</li>
+                                <li><b>Price: </b>${vehicle.dailyPrice}$/day</li>
+                                <li><b>Mileage: </b> ${vehicle.details.mileage} km</li>
                             </ul>
                         </div>
                         <div class="col-md-4">
                             <ul>
-                            <li><b>Number of Seats: </b><%= vehicle.details.seats %></li>
-                            <li><b>Number of Doors: </b><%= vehicle.details.doors %></li>
+                            <li><b>Number of Seats: </b>${vehicle.details.seats}</li>
+                            <li><b>Number of Doors: </b>${vehicle.details.doors}</li>
                             </ul>
                         </div>
                         <div class="col-md-4">
                             <ul>
-                                <li><b>Is Automatic? </b><%= vehicle.details.isAutomatic %></li>
-                                <li><b>Engine Type: </b> <%= vehicle.details.engineType %></li>
+                                <li><b>Is Automatic? </b>${vehicle.details.isAutomatic}</li>
+                                <li><b>Engine Type: </b>${vehicle.details.engineType}</li>
                             </ul>
                         </div>
                     </div>
