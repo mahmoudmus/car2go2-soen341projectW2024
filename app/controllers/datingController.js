f6891bb9abd7054ac4ec30c826339de8c9f91935
Royaleagle04 <55101369+Royaleagle04@users.noreply.github.com>
2024-04-09 11:12:40 -0400

    Commented out console logs used for debugging

--- app/controllers/datingController.js
+++ app/controllers/datingController.js
@@ -19,7 +19,6 @@ const Branch = require('../models/branch');
 */
 
 exports.createDatingProfile = asyncHandler(async (req, res, next) => {
-    //let typeProfile, categoryProfile, engineProfile, priceProfile, colourProfile, makeProfile, isAutomaticProfile;
     const {vehicleArray, startDate, endDate, branchName} = req.body;
     const newDatingProfile = new DatingProfile({ //Should be added as defaults in the model if possible
         categoryProfile: {
@@ -63,7 +62,7 @@ exports.createDatingProfile = asyncHandler(async (req, res, next) => {
     }
     try{
         buildDatingProfile(savedDatingProfile, vehicleArray);
-        console.log("Dating Profile: "+ savedDatingProfile);
+        //console.log("Dating Profile: "+ savedDatingProfile);
     } catch (e){
         res.status(400).send({ message: 'Could not update dating profile.' }); 
     }
@@ -112,13 +111,13 @@ async function buildDatingProfile(datingProfile, vehicleArray) {
 
 async function matchDate(datingProfile, startDate, endDate, branchName){
     const matchRate = 0;
-    console.log("Before findAvailableVehicles");
+    //console.log("Before findAvailableVehicles");
     const availableVehicles = await findAvailableVehicles(startDate, endDate, branchName);
-    console.log("After Available vehicles");
+    //console.log("After Available vehicles");
     var highestScore = 0;
     var matchedvehicle;
-    availableVehicles.forEach((vehicle) =>{
-        console.log("Make: " +vehicle.details.make);  //calculates the score for each vehicle. Can reduce scope of availableVehicles by first filtering for strict preferences (ie colour MUST be red)
+    availableVehicles.forEach((vehicle) =>{ //calculates the score for each vehicle. Can reduce scope of availableVehicles by first filtering for strict preferences (ie colour MUST be red). But then have to take into account case where query is empty.
+        //console.log("Make: " +vehicle.details.make);  
             let score = calculateMatchScore(vehicle, datingProfile);
             if(score>highestScore){
                 highestScore = score;
@@ -136,7 +135,7 @@ async function matchDate(datingProfile, startDate, endDate, branchName){
 function calculateMatchScore(vehicle, datingProfile){
     let score = 0;
     //logic here
-    console.log("Calculating Match Score");
+    //console.log("Calculating Match Score");
     const categoryScore = datingProfile.categoryProfile[vehicle.category];
     const typeScore = datingProfile.typeProfile[vehicle.type];
     const engineScore = datingProfile.engineProfile[vehicle.details.engineType];
@@ -144,19 +143,19 @@ function calculateMatchScore(vehicle, datingProfile){
     if(colourScore=="null"){  //null or undefined
         colourScore=0;
     }
-    console.log("Colour Score: "+colourScore);
+    //console.log("Colour Score: "+colourScore);
     const makeScore = datingProfile.makeProfile.get(vehicle.details.make);
     if(makeScore=="null"){ //null or undefined
         makeScore=0;
     }
-    console.log("Make Score: "+makeScore);
+    //console.log("Make Score: "+makeScore);
     const isAutomaticScore = datingProfile.isAutomaticProfile;
-    console.log("Automatic Score: "+isAutomaticScore);
+    //console.log("Automatic Score: "+isAutomaticScore);
     const priceScore = calculatePriceScore(vehicle, datingProfile);
-    console.log("Price Score: "+priceScore);
+    //console.log("Price Score: "+priceScore);
 
     let totalScore = (categoryScore + typeScore+ engineScore+ makeScore + isAutomaticScore +priceScore)/6;
-    console.log("Total Score: "+totalScore);
+    //console.log("Total Score: "+totalScore);
     if ((totalScore+colourScore)/2>totalScore){ //colour score is only added as bonus points if it makes a better match
         totalScore= (totalScore+colourScore)/2; 
     }
@@ -169,30 +168,30 @@ function calculateMatchScore(vehicle, datingProfile){
  */
 function calculatePriceScore(vehicle, datingProfile){
     //Uses min, max and avg from datingProfile to build a triangular distribution.
-    console.log("Calculating Price Score")
+    //console.log("Calculating Price Score")
     const min = datingProfile.priceProfile.min;
     const max = datingProfile.priceProfile.max;
     const peak = datingProfile.priceProfile.avg;
     const vehiclePrice = vehicle.dailyPrice;
-    console.log("min: " +min)
-    console.log("max: " +max)
-    console.log("peak: " +peak)
-    console.log("vehicle Price: " +vehiclePrice);
+    //console.log("min: " +min)
+    //console.log("max: " +max)
+    //console.log("peak: " +peak)
+    //console.log("vehicle Price: " +vehiclePrice);
     const scoreFactor = 50*(max-min); //Multiplication factor to get scores ranging from 0-100, where 100 if price=average=peak
     var PDF =0; // If outside bounds, PDF = 0
     //Triangle distribution PDF computation
     if(min<=vehiclePrice && vehiclePrice<=peak){
         PDF=2*(vehiclePrice-min)/((max-min)*(peak-min));
-        console.log("Case 1");
+        //console.log("Case 1");
     }else if(peak< vehiclePrice && vehiclePrice <=max){
         PDF=2*(max-vehiclePrice)/((max-min)*(max-peak));
-        console.log("Case 2");
+        //console.log("Case 2");
     }else{
         return 0
     }
-    console.log("PDF = " +PDF);
+    //console.log("PDF = " +PDF);
     const adjustedScale= 1/2*(PDF*scoreFactor)+50;
-    console.log("AdjustedScale = " +adjustedScale);
+    //console.log("AdjustedScale = " +adjustedScale);
     return adjustedScale;
 }
 
@@ -224,19 +223,19 @@ async function findAvailableVehicles(startDate, endDate, branchName) {
                 branch: branch._id
             }
         );
-        console.log("branch name: "+ branch.name);
+        //console.log("branch name: "+ branch.name);
     }
     
     
-    if(availableVehicles == "null"){
-        console.log("Available vehicles is null or undefined");
-    }else if(Object.keys(availableVehicles).length === 0){
-        console.log("Available vehicles is empty")
-    }
-    else{
-        console.log("Available vehicles is defined");
-        console.log("There are " +availableVehicles.length +" available vehicles");   
-    }
+    // if(availableVehicles == "null"){
+    //     console.log("Available vehicles is null or undefined");
+    // }else if(Object.keys(availableVehicles).length === 0){
+    //     console.log("Available vehicles is empty")
+    // }
+    // else{
+    //     console.log("Available vehicles is defined");
+    //     console.log("There are " +availableVehicles.length +" available vehicles");   
+    // }
 
     return availableVehicles;
 }
